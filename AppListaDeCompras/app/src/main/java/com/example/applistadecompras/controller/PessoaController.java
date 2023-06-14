2fc6271d17eb9b5b3c9419383a9121c98b84c15a
João Rocha <rocha.joao.victor.50@gmail.com>
2023-06-14 18:54:56 -0300

    App Lista De Compras

--- AppListaDeCompras/app/src/main/java/com/example/applistadecompras/controller/PessoaController.java
+++ AppListaDeCompras/app/src/main/java/com/example/applistadecompras/controller/PessoaController.java
@@ -1,4 +1,32 @@
 package com.example.applistadecompras.controller;
 
+import com.example.applistadecompras.model.Pessoa;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PessoaController {
+    private List listaCompras;
+
+    public PessoaController() {
+        listaCompras = new ArrayList();
+        lista();
+    }
+
+    public List<Pessoa> getListaCompras() {
+        return listaCompras;
+    }
+
+    private List lista() {
+        listaCompras = new ArrayList<Pessoa>();
+        listaCompras.add(new Pessoa("Informatica"));
+        listaCompras.add(new Pessoa("Informatica"));
+        listaCompras.add(new Pessoa("Informatica"));
+        listaCompras.add(new Pessoa("Informatica"));
+        listaCompras.add(new Pessoa("Informatica"));
+        listaCompras.add(new Pessoa("Informatica"));
+        listaCompras.add(new Pessoa("Informatica"));
+        listaCompras.add(new Pessoa("Informatica"));
+        return listaCompras;
+    }
 }
---
+++ AppListaDeCompras/app/src/main/java/com/example/applistadecompras/model/Pessoa.java
@@ -0,0 +1,21 @@
+package com.example.applistadecompras.model;
+
+public class Pessoa {
+    private String compradesejado;
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String compradesejado) {
+        this.compradesejado = compradesejado;
+    }
+
+    public String getCompradesejado() {
+        return compradesejado;
+    }
+
+    public void setCompradesejado(String compradesejado) {
+        this.compradesejado = compradesejado;
+    }
+}
